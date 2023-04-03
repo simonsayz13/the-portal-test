068017dd81de7846529ff7f133ddab38a7923da4
Simon Tan <simonsayz13@gmail.com>
2023-04-03 22:20:12 +0100

    Improved sortRouteV2 function Added - Added separate test case for correctness of each function. - Added test case for timing each function.

--- src/tests/application.test.ts
+++ src/tests/application.test.ts
@@ -1,6 +1,7 @@
-import { sortRoute } from "../utility/util";
+import { sortRouteV1, sortRouteV2 } from "../utility/util";
+import { StressTestData } from "../data/unit_test_data";
 
-test("Sorting route function should return a list of sorted route objects", () => {
+test("sortRouteV1 function should return the correct answer ", () => {
   const routeArray = [
     { product_code: "15248", quantity: "10", pick_location: "AZ 5" },
     { product_code: "25636", quantity: "1", pick_location: "AZ 4" },
@@ -8,9 +9,7 @@ test("Sorting route function should return a list of sorted route objects", () =
     { product_code: "25214", quantity: "10", pick_location: "F 2" },
     { product_code: "36389", quantity: "4", pick_location: "F 1" },
   ];
-
-  const sortedRoute = sortRoute(routeArray);
-
+  const sortedRoute = sortRouteV1(routeArray);
   expect(sortedRoute).toEqual([
     { product_code: "26982", quantity: "1", pick_location: "B 2" },
     { product_code: "36389", quantity: "4", pick_location: "F 1" },
@@ -18,4 +17,32 @@ test("Sorting route function should return a list of sorted route objects", () =
     { product_code: "25636", quantity: "1", pick_location: "AZ 4" },
     { product_code: "15248", quantity: "10", pick_location: "AZ 5" },
   ]);
+});
+
+test("sortRouteV2 function should return the correct answer ", () => {
+  const routeArray = [
+    { product_code: "15248", quantity: "10", pick_location: "AZ 5" },
+    { product_code: "25636", quantity: "1", pick_location: "AZ 4" },
+    { product_code: "26982", quantity: "1", pick_location: "B 2" },
+    { product_code: "25214", quantity: "10", pick_location: "F 2" },
+    { product_code: "36389", quantity: "4", pick_location: "F 1" },
+  ];
+  const sortedRoute = sortRouteV2(routeArray);
+  expect(sortedRoute).toEqual([
+    { product_code: "26982", quantity: "1", pick_location: "B 2" },
+    { product_code: "36389", quantity: "4", pick_location: "F 1" },
+    { product_code: "25214", quantity: "10", pick_location: "F 2" },
+    { product_code: "25636", quantity: "1", pick_location: "AZ 4" },
+    { product_code: "15248", quantity: "10", pick_location: "AZ 5" },
+  ]);
+});
+
+test("Stress test and comparing different sort comparators", () => {
+  console.time("sortRouteV1");
+  sortRouteV1(StressTestData);
+  console.timeEnd("sortRouteV1");
+
+  console.time("sortRouteV2");
+  sortRouteV2(StressTestData);
+  console.timeEnd("sortRouteV2");
 });
--- src/utility/util.ts
+++ src/utility/util.ts
@@ -2,7 +2,7 @@ import { createObjectCsvWriter } from "csv-writer";
 import { ItemData } from "../types/types";
 import { RESULT_FILE } from "../config/constants";
 
-export function sortRoute(inputArray: Array<ItemData>): Array<ItemData> {
+export function sortRouteV1(inputArray: Array<ItemData>): Array<ItemData> {
   return inputArray.sort((itemA: ItemData, itemB: ItemData) => {
     const itemABay: string = itemA.pick_location.split(" ")[0].toLowerCase();
     const itemBBay: string = itemB.pick_location.split(" ")[0].toLowerCase();
@@ -32,6 +32,34 @@ export function sortRoute(inputArray: Array<ItemData>): Array<ItemData> {
     }
     return 0;
   });
+}
+
+export function sortRouteV2(inputArray: Array<ItemData>): Array<ItemData> {
+  inputArray.sort((itemA: ItemData, itemB: ItemData) => {
+    const [itemABay, itemAShelf] = itemA.pick_location.split(" ");
+    const [itemBBay, itemBShelf] = itemB.pick_location.split(" ");
+
+    if (itemABay.length < itemBBay.length) {
+      return -1;
+    }
+    if (itemABay.length > itemBBay.length) {
+      return 1;
+    }
+    if (itemABay < itemBBay) {
+      return -1;
+    }
+    if (itemABay > itemBBay) {
+      return 1;
+    }
+    if (itemAShelf < itemBShelf) {
+      return -1;
+    }
+    if (itemAShelf > itemBShelf) {
+      return 1;
+    }
+    return 0;
+  });
+  return inputArray;
 }
 
 export function writeResult(resultArray: Array<ItemData>): void {
